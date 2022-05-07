d6178de1f86e10dd5a4a61db552563cc9ce3a3cc
MubarekSD <mubareksd@gmail.com>
2022-05-07 20:39:21 +0300

    24. Starting out with Expressions

--- 1-variables-datatypes-operators/HelloWorld/src/Main.java
+++ 1-variables-datatypes-operators/HelloWorld/src/Main.java
@@ -4,7 +4,12 @@ class Main {
         System.out.println("Hello, Mubarek");
 
         int myFirstNumber = (10 + 5) + (2 * 10);
-        System.out.println(myFirstNumber);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber +myThirdNumber;
+        System.out.println(myTotal);
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
 
     }
 
