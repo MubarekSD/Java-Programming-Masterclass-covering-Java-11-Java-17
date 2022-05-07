9549913cffb484536dee1d359d2df6becda716e0
MubarekSD <mubareksd@gmail.com>
2022-05-07 20:05:39 +0300

    23. Variables

--- 1-variables-datatypes-operators/HelloWorld/src/Main.java
+++ 1-variables-datatypes-operators/HelloWorld/src/Main.java
@@ -2,6 +2,10 @@ class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, Mubarek");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        System.out.println(myFirstNumber);
+
     }
 
 }
